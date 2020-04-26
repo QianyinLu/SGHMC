715e27ff96a73bd3245247c096e984afaa1ee13b
QianyinLu <54278344+QianyinLu@users.noreply.github.com>
2020-04-26 13:06:50 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="SGHMC", 
+    version="0.1",
+    author="Qianyin Lu, Zewen Zhang",
+    description="Algorithm of Stochastic Gradient Hamiltonian Monte Carlo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['sghmc'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
